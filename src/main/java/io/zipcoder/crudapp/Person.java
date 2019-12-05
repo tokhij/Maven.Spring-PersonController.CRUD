bbc9a07cd4c9b972f422da16f93022fd4eccf2f1
John Tokhi <56596400+tokhij@users.noreply.github.com>
2019-12-05 16:27:14 -0500

    started

---
+++ src/main/java/io/zipcoder/crudapp/Person.java
@@ -0,0 +1,47 @@
+package io.zipcoder.crudapp;
+
+public class Person {
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Person(){
+    }
+
+    public Person(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(Long id, String firstName, String lastName){
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+
+}
